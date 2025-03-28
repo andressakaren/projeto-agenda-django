fe08c70ee50afb304cf6581463e3e1c24890c1e4
Andressa Karen <lqsandressa@gmail.com>
2025-03-28 08:55:47 -0300

    Filtrando valores com Q e OR para o campo de pesquisa

--- contact/urls.py
+++ contact/urls.py
@@ -5,5 +5,6 @@ app_name = 'contact' #namespace
 
 urlpatterns = [
     path('<int:contact_id>/', views.contact, name='contact'), 
+    path('search/', views.search, name='search'), 
     path('', views.index, name='index'), 
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from contact.models import Contact
-# Create your views here.
+from django.db.models import Q
 
 
 def index(request):
@@ -8,11 +8,42 @@ def index(request):
         .filter(show=True)\
         .order_by('-id')[0:10]
 
-    print(contacts.query)
+    # print(contacts.query)
 
     context = {
         'contacts': contacts,
         'site_title': 'Contatos - ',
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+    
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    
+    # print(search_value)
+    
+    if search_value == '':
+        return redirect('contact:index')
+    
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter (
+            Q(first_name__icontains=search_value) | 
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value) 
+            )\
+        .order_by('-id')
+
+    # print(contacts.query)
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - ',
     }
 
     return render(
